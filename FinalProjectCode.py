69967c30365871f1ca476448eccf3aeb70020a98
Cmdr-Raygun <renttibwerdna@gmail.com>
2024-12-13 15:56:15 -0600

    Adding fields
    Fields and info tentatively added to database. Other WIP code is present but commented out.

--- FinalProjectCode.py
+++ FinalProjectCode.py
@@ -2,31 +2,39 @@
 import sqlite3
 #main function
 def main():
-  #connect to database
-  connection = sqlite3.connect('info.db')
-  #get cursor
-  cursor = connection.cursor()
-  #add table
-  info_table(cursor)
-  #commit changes and close connection
-  connection.commit()
-  connection.close()
+    #connect to database
+    connection = sqlite3.connect('info.db')
+    #get cursor
+    cursor = connection.cursor()
+    #add table
+    info_table(cursor)
+    #commit changes and close connection
+    connection.commit()
+    connection.close()
   
 def info_table(cursor):
-  #if table exists, drop it
-  cursor.execute('DROP TABLE IF EXISTS Info')
-  #create table
-  cursor.execute('''CREATE TABLE Info (IDNum INTEGER PRIMARY KEY NOT NULL, Name TEXT)''')
-  #add rows to table
-  info_rows = [(1,'Glu Tton'),
-              (2,'Guill Otine'),
-              (3,'Pedalto Themedal'),
-              (4,'Badprog Rammer'),
-              (5,'Sadn Ess')]
-    cursor.executemany("insert into info values (?,?)", info_rows)
+    #if table exists, drop it
+    cursor.execute('DROP TABLE IF EXISTS YearLookup')
+    cursor.execute('DROP TABLE IF EXISTS Info')
+    #create table
+    cursor.execute('''CREATE TABLE YearLookup (YearID INTEGER PRIMARY KEY NOT NULL, YearName TEXT)''')
+    cursor.execute('''CREATE TABLE Info (IDNum INTEGER PRIMARY KEY NOT NULL, Name TEXT, YearID, Email TEXT, PhoneNum TEXT, Address TEXT, GPA DECIMAL, FOREIGN KEY(YearID) REFERENCES YearLookup(YearID))''')
+    #add rows to table
+    info_rows = [(1, 'Freshman'), (2, 'Sophomore'), (3, 'Junior'), (4, 'Senior')]
+    cursor.executemany("insert into YearLookup values (?,?)", info_rows)
+    info_rows = [(1,'Glu Tton', 1, 'glutton.free@college.edu', '612-648-8420', 'McDonald\'s, 1480 85th Ave N, Brooklyn Park, MN 55444', 3.7),
+              (2,'Guill Otine', 3, 'frenchrev1789@college.edu', '763-501-6629', 'Palais Bourbon, 126 Rue de l\'Université, 75007 Paris, France', 2.8),
+              (3,'Pedalto Themedal', 2, 'gottagofast@college.edu', '612-295-4398', 'Sumitomo Fudosan Osaki Garden Tower 9F, 1-1-1 Nishi-Shinagawa, Shinagawa-ku, Tokyo 141-0033, Japan', 3.3),
+              (4,'Badprog Rammer', 4, 'whoneedscomments@college.edu', '952-254-8163', 'One Apple Park Way, Cupertino, CA 95014', 1.9),
+              (5,'Sadn Ess', 2, 'icanteven@college.edu', '763-892-5678', '1200 Grand Central Ave, Glendale, CA 91201', 3.1)]
+    cursor.executemany("insert into info values (?,?,?,?,?,?,?)", info_rows)
 
     #print contents of database
     print("Here are the current contents of the database:")
+
+    # for row in cursor.execute('''SELECT Info.IDNum, Info.Name, YearLookup.YearName, Info.Email, Info.PhoneNum, Info.Address, Info.GPA FROM YearLookup, Info WHERE YearLookup.IDNum == Info.YearNum'''):
+    #     print(row)
+
     for row in cursor.execute("select * from info"):
         print(row)
 
